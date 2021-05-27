31ce46f349a6a8f725f721bc844c6bcfa97da23b
ThomasDravigney <56030910+ThomasDravigney@users.noreply.github.com>
2021-05-27 12:13:54 +0200

    Create security_agent

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from models.post import Post
+from models.security_agent import SecurityAgent
 
 server = Flask(__name__, template_folder='views')
 
@@ -15,6 +16,12 @@ def index():
     }
     return render_template('index.html', model=data)
 
+
+
+@server.route('/security_agent')
+def security_agent():
+    data = SecurityAgent('Thomas', 'Dravigney')
+    return render_template('security_agent.html', model=data)
 
 if __name__ == '__main__':
     server.run()
---
+++ models/security_agent.py
@@ -0,0 +1,4 @@
+class SecurityAgent:
+    def __init__(self, firstName, lastName):
+        self.firstName = firstName
+        self.lastName = lastName
